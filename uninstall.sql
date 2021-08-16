aab2fff18c7e15f7485f71c9715969279fe7aa00
tomgilabert <tom.gilabert@gmail.com>
2021-08-16 15:49:15 +0200

    Drop table bosssecretary_group_numbers_free
    Drop table bosssecretary_group_numbers_free

--- uninstall.sql
+++ uninstall.sql
@@ -2,5 +2,6 @@ DROP TABLE `bosssecretary_boss`;
 DROP TABLE `bosssecretary_secretary`;
 DROP TABLE `bosssecretary_group`;
 DROP TABLE `bosssecretary_chief`;
+DROP TABLE `bosssecretary_group_numbers_free`;
 
 
